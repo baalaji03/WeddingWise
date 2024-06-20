b88a49e3f4e5652b37bbfc28629b60307892d16e
baalaji03 <rps123a@gmail.com>
2024-06-20 00:59:08 -0700

    design

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
@@ -6,15 +6,6 @@ import { Button, Navbar, NavbarCollapse, NavbarLink, TextInput } from 'flowbite-
 
 const Header = () => {
     const path = useLocation().pathname;
-    const [hoveredItem, setHoveredItem] = useState(null);
-
-    const handleMouseEnter = (item) => {
-        setHoveredItem(item);
-    };
-
-    const handleMouseLeave = () => {
-        setHoveredItem(null);
-    };
 
     return (
         <Navbar className='border-b-2'>
@@ -41,77 +32,58 @@ const Header = () => {
                 </Button>
                 <Navbar.Toggle />
             </div>
-            <NavbarCollapse>
-                <div className='flex flex-col lg:flex-row'>
-                    <div
-                        className={`relative group ${hoveredItem && hoveredItem !== 'PlanningTool' ? 'hidden' : ''}`}
-                        onMouseEnter={() => handleMouseEnter('PlanningTool')}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                        <Navbar.Link active={path === '/PlanningTool'} as={'div'}>
-                            <Link to='/PlanningTool'>PlanningTool</Link>
-                        </Navbar.Link>
-                        <div className="absolute left-0 top-full hidden group-hover:flex flex-col mt-2 bg-white shadow-lg">
-                            <Link to='/PlanningTool/Budget' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Budget</Link>
-                            <Link to='/PlanningTool/Outdoor' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Outdoor Venues</Link>
-                            <Link to='/PlanningTool/Destination' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Destination Venues</Link>
-                        </div>
+            <NavbarCollapse >
+                <div className='relative group'>
+                
+                    <Navbar.Link active={path === '/PlanningTool'} as={'div'}>
+                        <Link to='/PlanningTool'>PlanningTool</Link>
+                    </Navbar.Link>
+                  
+                    <div className=" left-0 top-full hidden group-hover:flex flex-col mt-2 bg-white shadow-lg">
+                        <Link to='/PlanningTool/Budget' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Budget</Link>
+                        <Link to='/PlanningTool/Outdoor' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Outdoor Venues</Link>
+                        <Link to='/PlanningTool/Destination' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Destination Venues</Link>
                     </div>
-                    <div
-                        className={`relative group ${hoveredItem && hoveredItem !== 'WeddingVendor' ? 'hidden' : ''}`}
-                        onMouseEnter={() => handleMouseEnter('WeddingVendor')}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                        <Navbar.Link active={path === '/WeddingVendor'} as={'div'}>
-                            <Link to='/WeddingVendor'>WeddingVendor</Link>
-                        </Navbar.Link>
-                        <div className="absolute left-0 top-full hidden group-hover:flex flex-col mt-2 bg-white shadow-lg">
-                            <Link to='/WeddingVendor/WeddingPhotographers' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Wedding Photographers</Link>
-                            <Link to='/WeddingVendor/WeddingVideographers' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Wedding Videographers</Link>
-                            <Link to='/WeddingVendor/WeddingPlanners' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Wedding Planners</Link>
-                            <Link to='/WeddingVendor/Caterers' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Caterers</Link>
-                        </div>
+                   
+                </div>
+                <div className='relative group'>
+                    <Navbar.Link active={path === '/WeddingVendor'} as={'div'}>
+                        <Link to='/WeddingVendor'>WeddingVendor</Link>
+                    </Navbar.Link>
+                    <div className=" left-0 top-full hidden group-hover:flex flex-col mt-2 bg-white shadow-lg">
+                        <Link to='/WeddingVendor/WeddingPhotographers' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Wedding Photographers</Link>
+                        <Link to='/WeddingVendor/WeddingVideographers' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Wedding Videographers</Link>
+                        <Link to='/WeddingVendor/WeddingPlanners' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Wedding Planners</Link>
+                        <Link to='/WeddingVendor/Caterers' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Caterers</Link>
                     </div>
-                    <div
-                        className={`relative group ${hoveredItem && hoveredItem !== 'WeddingVenue' ? 'hidden' : ''}`}
-                        onMouseEnter={() => handleMouseEnter('WeddingVenue')}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                        <Navbar.Link active={path === '/WeddingVenue'} as={'div'}>
-                            <Link to='/WeddingVenue'>WeddingVenue</Link>
-                        </Navbar.Link>
-                        <div className="absolute left-0 top-full hidden group-hover:flex flex-col mt-2 bg-white shadow-lg">
-                            <Link to='/WeddingVenue/Indoor' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Banquet Halls</Link>
-                            <Link to='/WeddingVenue/Outdoor' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Wedding Resorts</Link>
-                            <Link to='/WeddingVenue/Destination' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Kalyana Mandapams</Link>
-                        </div>
+                </div>
+                <div className='relative group'>
+                    <Navbar.Link active={path === '/WeddingVenue'} as={'div'}>
+                        <Link to='/WeddingVenue'>WeddingVenue</Link>
+                    </Navbar.Link>
+                    <div className=" left-0 top-full hidden group-hover:flex flex-col mt-2 bg-white shadow-lg">
+                        <Link to='/WeddingVenue/Indoor' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Banquet Halls</Link>
+                        <Link to='/WeddingVenue/Outdoor' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Wedding Resorts</Link>
+                        <Link to='/WeddingVenue/Destination' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Kalyana Mandapams</Link>
                     </div>
-                    <div
-                        className={`relative group ${hoveredItem && hoveredItem !== 'Bride' ? 'hidden' : ''}`}
-                        onMouseEnter={() => handleMouseEnter('Bride')}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                        <Navbar.Link active={path === '/Bride'} as={'div'}>
-                            <Link to='/Bride'>Bride</Link>
-                        </Navbar.Link>
-                        <div className="absolute left-0 top-full hidden group-hover:flex flex-col mt-2 bg-white shadow-lg">
-                            <Link to='/Bride/MehndiArtists' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Mehndi Artists</Link>
-                            <Link to='/Bride/BridalMakeupArtists' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Bridal Makeup Artists</Link>
-                            <Link to='/Bride/BridalJewellery' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Bridal Jewellery</Link>
-                        </div>
+                </div>
+                <div className='relative group'>
+                    <Navbar.Link active={path === '/Bride'} as={'div'}>
+                        <Link to='/Bride'>Bride</Link>
+                    </Navbar.Link>
+                    <div className=" left-0 top-full hidden group-hover:flex flex-col mt-2 bg-white shadow-lg">
+                        <Link to='/Bride/MehndiArtists' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Mehndi Artists</Link>
+                        <Link to='/Bride/BridalMakeupArtists' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Bridal Makeup Artists</Link>
+                        <Link to='/Bride/BridalJewellery' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Bridal Jewellery</Link>
                     </div>
-                    <div
-                        className={`relative group ${hoveredItem && hoveredItem !== 'Groom' ? 'hidden' : ''}`}
-                        onMouseEnter={() => handleMouseEnter('Groom')}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                        <Navbar.Link active={path === '/Groom'} as={'div'}>
-                            <Link to='/Groom'>Groom</Link>
-                        </Navbar.Link>
-                        <div className="absolute left-0 top-full hidden group-hover:flex flex-col mt-2 bg-white shadow-lg">
-                            <Link to='/Groom/Sherwani' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sherwani</Link>
-                            <Link to='/Groom/GroomMakeupArtists' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Groom Makeup Artists</Link>
-                        </div>
+                </div>
+                <div className='relative group'>
+                    <Navbar.Link active={path === '/Groom'} as={'div'}>
+                        <Link to='/Groom'>Groom</Link>
+                    </Navbar.Link>
+                    <div className="left-0 top-full hidden group-hover:flex flex-col mt-2 bg-white shadow-lg">
+                        <Link to='/Groom/Sherwani' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sherwani</Link>
+                        <Link to='/Groom/GroomMakeupArtists' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Groom Makeup Artists</Link>
                     </div>
                 </div>
             </NavbarCollapse>
