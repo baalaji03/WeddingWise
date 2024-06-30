026ecea60030d6f13aa14afbc35e644394ff2de2
baalaji03 <rps123a@gmail.com>
2024-06-30 03:07:48 -0700

    update5

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -151,7 +151,7 @@ const Home = () => {
     <br />
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Make your first impression unforgettable with our elegant and unique wedding invitation cards!</p>
     <br />
-    <Link to="/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <Link to="/invitation" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
     Explore cards
         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
