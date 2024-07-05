063314252b5b02bed4ab9828a54f83c3c7ae5ffe
baalaji03 <rps123a@gmail.com>
2024-07-04 20:45:20 -0700

    FRONT_Completed

--- src/Components/OAuth.jsx
+++ src/Components/OAuth.jsx
@@ -18,7 +18,7 @@ const OAuth = () => {
     provider.setCustomParameters({ prompt:"select_account" });
     try {
       const result = await signInWithPopup(auth, provider);
-      const res = await fetch("http://localhost:5000/api/auth/google", {
+      const res = await fetch("https://wedding-event-frontend.netlify.app/google", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
