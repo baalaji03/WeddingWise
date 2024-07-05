1d0b17cd3c75b11d4be5e609ced840ff1522fb4a
baalaji03 <rps123a@gmail.com>
2024-07-04 21:50:24 -0700

    upp

--- src/Components/OAuth.jsx
+++ src/Components/OAuth.jsx
@@ -18,7 +18,7 @@ const OAuth = () => {
     try {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
-      const res = await fetch("https://wedding-event-frontend.netlify.app/api/auth/google", {
+      const res = await fetch("https://wedding-event-backend.netlify.app/api/auth/google", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
