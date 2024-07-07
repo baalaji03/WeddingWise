4adce7aa9008a6810ab4adfdea1aa56e9d7e139b
baalaji03 <rps123a@gmail.com>
2024-07-06 22:05:28 -0700

    Corrected

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -25,7 +25,7 @@ const Signup = () => {
       try {
           setLoading(true);
           setErrorMessage(null);
-          const response = await fetch('https://backend-wedding-project.netlify.app/api/auth/register-user',{
+          const response = await fetch('https://wedding-backend-project.netlify.app/api/auth/register-user',{
               method:'POST',
               headers:{
                   'Content-Type':'application/json'
