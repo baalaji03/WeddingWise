138ad30c5aaa1a8c70def582eeae54ec64d64d99
baalaji03 <rps123a@gmail.com>
2024-07-17 15:37:04 -0700

    done

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -26,7 +26,7 @@ const Signin = () => {
     }
     try {
         dispatch(signInStart())
-        const response = await fetch('http://localhost:8888/api/auth/login-user',{
+        const response = await fetch('https://backend-wedding-project.onrender.com/api/auth/login-user',{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -25,7 +25,7 @@ const Signup = () => {
       try {
           setLoading(true);
           setErrorMessage(null);
-          const response = await fetch('http://localhost:8888/api/auth/register-user',{
+          const response = await fetch('https://backend-wedding-project.onrender.com/api/auth/register-user',{
               method:'POST',
               headers:{
                   'Content-Type':'application/json'
